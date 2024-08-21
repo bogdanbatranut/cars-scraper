bc100d64b6af9483163b7fd7e409828ea364e66a
Bogdan Batranut <bogdan.batranut@plentysystems.com>
2024-08-21 22:34:09 +0300

    Add bmw.de market

--- pkg/sessionstarter/session.starter.service.go
+++ pkg/sessionstarter/session.starter.service.go
@@ -106,7 +106,7 @@ func inArrayUINT(str uint, list []uint) bool {
 func (sss SessionStarterService) createSessionJobs(sessionID uuid.UUID) []jobs.SessionJob {
 	sessionJobs := []jobs.SessionJob{}
 
-	markets := []uint{9, 11, 12, 13, 14, 15, 16, 17, 18, 19}
+	markets := []uint{9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	//criterias := []uint{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}
 	//markets := []uint{9}
 	criterias := []uint{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31}
